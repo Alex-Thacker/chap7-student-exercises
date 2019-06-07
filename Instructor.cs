d8d42a844ee4199a5ac097d1bf694c8794104c3e
Alex Thacker <thacker.alex@yahoo.com>
2019-06-07 14:28:31 -0500

    part 3 done

--- Instructor.cs
+++ Instructor.cs
@@ -2,11 +2,7 @@ using System;
 using System.Collections.Generic;
 
 namespace student_exercises {
-    public class Instructor {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string SlackHandle { get; set; }
-        public Cohort Cohort { get; set; }
+    public class Instructor : NSSPerson {
         public string Specialty { get; set; }
         public void Assign (Student variable, Exercise exercise, Exercise exercise1) {
             variable.Exercises.Add(exercise);
---
+++ NSSPerson.cs
@@ -0,0 +1,10 @@
+namespace student_exercises
+{
+    public class NSSPerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string SlackHandle { get; set; }
+        public Cohort Cohort { get; set; }
+    }
+}
